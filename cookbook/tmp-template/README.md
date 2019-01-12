eaaeb6e0e62ee394d48a20adc59deaf2d53ea49f
Theo Armour <theo-armour@users.noreply.github.com>
2019-01-12 14:19:34 -0800

    add tmp-template test files

---
+++ cookbook/tmp-template/README.md
@@ -0,0 +1,56 @@
+
+<span style=display:none; >[You are now in a GitHub source code view - click this link to view Read Me file as a web page]( https://pushme-pullyou.github.io/tootoo13/#cookbook/tmp-template/README.md "View file as a web page." ) </span>
+
+<div><input type=button class = "btn btn-secondary btn-sm" onclick=window.location.href="https://github.com/pushme-pullyou/pushme-pullyou.github.io/blob/master/xxxxx/README.md"
+value="You are now in a GitHub web page view - Click this button to view this read me file as source code" ></div>
+
+<br>
+
+# [TooToo Template Read Me]( tootoo13/#cookbook/tmp-template/README.md )
+
+<!--
+<iframe src=https://pushme-pullyou.github.io/xxxxx/xxxxx.html width=100% height=500px >Iframes are not viewable in GitHub source code views</iframe>
+_<small>TooToo Template</small>_
+
+## Full Screen: [TooToo Template]( https://pushme-pullyou.github.io/xxxxx/xxxxx.html )
+-->
+
+
+## Concept
+
+* Provides default description text template
+* Provides default current status text template
+* Includes basic code to toggle display of current status
+* Provides sample content to help with examining CSS effects
+
+## To Do / Wish List
+
+
+## Issues
+
+
+## Things you can do using this script
+
+* Click the three bars( 'hamburger menu icon' ) to slide the menu in and out
+* Click the Octocat icon to view or edit the source code on GitHub
+* Click on title to reload
+* Press Control-U/Command-Option-U to view the source code
+* Press Control-Shift-J/Command-Option-J to see if the JavaScript console reports any errors
+
+
+## Links of Interest
+
+
+
+## Change Log
+
+### 2019-01-12 ~ Theo
+
+* First commit
+* Add cookbook HTML test script and read me file
+
+
+***
+
+# <center title="hello!" ><a href=javascript:window.scrollTo(0,0); style=text-decoration:none; > ❦ </a></center>
+
---
+++ cookbook/tmp-template/tmp-template.js
@@ -0,0 +1,98 @@
+// Copyright 2019 pushMe-pullYou authors. MIT License
+/* global  * /
+/* jshint esversion: 6 */
+
+
+
+const TMP = { "release": "R13.1", "date": "2019-01-12" };
+
+ TMP.description = TMP.description || document.head.querySelector( "[ name=description ]" ).content;
+
+TMP.currentStatus =
+	`
+		<h3> TooToo Template ${ TMP.release} ~ ${ TMP.date }</h3>
+
+		<p>
+			${ TMP.description }
+		</p>
+
+
+		<p>
+		Concept
+			<ul>
+				<li>Provides default description text template</li>
+				<li>Provides default current status text template</li>
+				<li>Includes basic code to toggle display of current status</li>
+				<li>Provides sample content to help with examining CSS effects</li>
+				<!-- <li></li> -->
+			</ul>
+		</p>
+
+
+		<p>
+			Change log
+			<ul>
+				<li>2019-01-12 ~ Add cookbook HTML test script and read me file</li>
+				<!-- <li></li> -->
+			</ul>
+		</p>
+
+		`;
+
+
+
+TMP.getMenuTemplate = function() {
+
+
+	const htm =
+	`
+		<details >
+
+			<summary>TooToo Template
+				<a id=tmpSum class=helpItem href="JavaScript:MNU.setPopupShowHide(tmpSum,TMP.currentStatus);" >&nbsp; ? &nbsp;</a>
+			</summary>
+
+			<div>
+
+			<p><a href="" >breadcrumb</a> &raquo; </p>
+
+			<h2><a href="" >Title H2</a></h2>
+
+			<div id=divRowButtonsStyle ></div>
+
+			<p><i>111 222 333 444 555</i></p>
+
+			<div>
+				text in a div aaa bbb ccc ddd eee fff
+			</div>
+
+			<p>
+				<label><b>Last Name</b></label>
+				<input type="text" style=width:100px; >
+			</p>
+
+			<p><button >button 1</button></p>
+
+			<p><button >button 2</button></p>
+
+			<select size=3 >
+				<option value="" disabled selected>Choose your option</option>
+				<option value="1">Option 1</option>
+				<option value="2">Option 2</option>
+				<option value="3">Option 3</option>
+			</select>
+
+
+				<p>
+					lorem ipsum, <a href="https://example.com" target="_blank">quia dolor sit</a>, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+				</p>
+
+			</div>
+
+		</details>
+	`;
+
+	return htm;
+
+
+};
