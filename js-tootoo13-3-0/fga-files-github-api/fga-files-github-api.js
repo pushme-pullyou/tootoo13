cc0f125915c9f4cc7525fef56b97bb04275cdbcd
Theo Armour <theo-armour@users.noreply.github.com>
2019-04-14 18:07:50 -0700

    tt r13-3-0

---
+++ js-tootoo13-3-0/fga-files-github-api/fga-files-github-api.js
@@ -0,0 +1,260 @@
+//Copyright 2019 Ladybug Tools authors. MIT License
+/* globals FIL */
+/* jshint esversion: 6 */
+/* jshint loopfunc: true */
+
+
+const FGA = { "release": "1.0.0", "date": "2019-04-13" };
+
+
+FGA.description =
+	`
+		Use GitHub API to obtain a list of files in a GitHub repo. Build menu to access the files
+	`;
+
+FGA.uriDefaultFile = "README.md";
+FGA.user = 'pushme-pullyou';
+FGA.repo = 'tootoo13';
+FGA.pathRepo = '';
+FGA.branch = '/master/';
+FGA.urlGitHubPage = "../../";
+
+FGA.urlSourceCode = `https://github.com/${ FGA.user}/${ FGA.repo }/`;
+FGA.urlSourceCodeImage = "https://pushme-pullyou.github.io/github-mark-64.png";
+FGA.iconInfo = `<img src="${ FGA.urlSourceCodeImage }" height=18 style=opacity:0.5 >`;
+
+FGA.xhr = new XMLHttpRequest(); // declare now to load event listeners in other modules
+FGA.regexImages = /\.(jpe?g|png|gif|webp|ico|svg|bmp)$/i;
+FGA.regexHtml = /\.(htm?l)$/i;
+
+FGA.contentsCss = `box-sizing: border-box; border: 1px solid #888; height: ${ window.innerHeight - 4 }px; margin: 0; padding:0; width:100%;`;
+
+
+
+
+FGA.getMenuFilesGithubApi = function() {
+
+	FGA.urlGitHubSource = "https://github.com/" + FGA.user + "/" + FGA.repo;
+	FGA.urlGitHubApiContents = 'https://api.github.com/repos/' + FGA.user + "/" + FGA.repo + '/contents/' + FGA.pathRepo;
+	FGA.accessToken = localStorage.getItem( 'githubAccessToken' ) || '';
+
+	const htm =
+		`
+			<details open >
+
+				<summary id=FGAsumSurfaces  >All folders and files
+					<a id=FGAsum class=helpItem href="JavaScript:MNU.setPopupShowHide(FGAsum,'README.md');" >&nbsp; ? &nbsp;</a>
+				</summary>
+
+				<div id = "FGAdivFilesGithubApi" ></div>
+
+				<div id = "FGAdivBreadcrumbs" ></div>
+
+				<div id = "FGAdivMenuItems" ></div>
+
+			</details>
+
+		`;
+
+	return htm;
+
+};
+
+
+
+FGA.getFiles = function() {
+
+	const timeStart = performance.now();
+
+	const url = !location.hash ? FGA.uriDefaultFile : location.hash.slice( 1 );
+	FGA.url = url;
+
+	//const ulc = url.toLowerCase();
+
+	const crumbs = url.slice( FGA.urlGitHubPage.length );
+	let pathCurrent = crumbs.lastIndexOf( '/' ) > 0 ? crumbs.slice( 0, crumbs.lastIndexOf( '/' ) ) : '';
+	//console.log( 'pathCurrent', pathCurrent );
+
+	if ( FGA.urlGitHubApiContents ){ FGA.setMenuGitHubPathFileNames( pathCurrent ); }
+
+};
+
+
+
+FGA.setMenuGitHubPathFileNames = function( path = '' ) {
+
+	//console.log( 'path', path );
+
+	const str = FGA.accessToken ? "?access_token=" + FGA.accessToken : "";
+
+	FGA.urlGitHubApiContents = `https://api.github.com/repos/${ FGA.user }/${ FGA.repo }/contents/${FGA.pathRepo }`;
+
+	const url = FGA.urlGitHubApiContents + path + str;
+
+	FGA.requestFile( url ); // to do: make request only once and triage thereafter
+
+	FGA.setBreadcrumbs( path );
+
+};
+
+
+
+FGA.requestFile = function( url ) {
+
+	xhr = new XMLHttpRequest();
+	xhr.open( 'GET', url, true );
+	xhr.onerror = function( xhr ) { console.log( 'error:', xhr  ); };
+	//xhr.onprogress = function( xhr ) { console.log(  'bytes loaded: ' + xhr.loaded.toLocaleString() ) }; /// or something
+	xhr.onload = FGA.callbackGitHubPathFileNames
+	xhr.send( null );
+
+};
+
+
+
+FGA.callbackGitHubPathFileNames = function( xhr ) {
+
+	const response = xhr.target.response;
+	const items = JSON.parse( response );
+	//console.log( 'items ', items );
+	FGA.menuItems = items;
+
+	//if ( items.message !== "Not Found" ) { alert( items.message ); return; }
+	if ( items.message ) { console.log( 'error', items.message ); return; } //breadcrumbs??
+
+	const htmFolders = FGA.getFoldersFromContents( items );
+
+	const htmFiles = FGA.getFilesFromContents( items );
+
+	const htmHelp =
+	`
+		<p>Click any <a href=${ FGA.urlSourceCode } >${ FGA.iconInfo }</a> icon to view source code on GitHub.
+
+		<p>Click any &#x2750; icon to go full screen & obtain a link to the individual file.</p>
+
+		<p>Tooltips provide file size.</p>
+	`;
+
+
+	FGAdivMenuItems.innerHTML = htmFolders + htmFiles + htmHelp;
+
+};
+
+
+
+FGA.getFoldersFromContents = function( items ) {
+
+	let htm = "";
+
+	const len = FGA.pathRepo.length;
+
+	const ignoreFolders = [ "0-templates-readme","archive","data",".github" ]; // turn into global
+
+	for ( let item of items ) {
+
+		if ( item.type === "dir" && !ignoreFolders.includes( item.name ) ) {
+
+			// why does this not work? item.path.split( "/" ).pop()
+			htm +=
+			`
+				<div style=margin-bottom:8px;padding:0; >
+					<a href=JavaScript:FGA.setMenuGitHubPathFileNames("${ item.path.slice( len ) }"); >
+						&#x1f4c1; ${ item.name }
+					</a>
+				</div>
+			`;
+
+		}
+
+	}
+
+	return htm;
+
+};
+
+
+
+FGA.getFilesFromContents = function( items ) {
+
+	let htm = "";
+
+	const len = FGA.pathRepo.length;
+
+	const ignoreFiles = [ ".gitattributes", ".gitignore", ".nojekyll", "404.html" ];
+
+	const name = FGA.url.split( "/" ).pop();
+
+	for ( let item of items ) {
+
+		if ( item.type === "file" && !ignoreFiles.includes( item.name ) ) {
+
+			const itemPath = encodeURI( item.path.slice( len ) );
+
+			const str = item.path.endsWith( "html" ) ? `<a href="${ FGA.urlGitHubPage }${ FGA.pathRepo }${ itemPath }" >&#x2750;</a>` : "";
+
+			const stl = item.name === name? "yellow" : "";
+
+			htm += // try grid or flexbox??
+			`
+				<table ><tr>
+					<td>
+						<a href="${ FGA.urlGitHubSource }/blob${ FGA.branch }${ itemPath }" target=_top >
+							${ FGA.iconInfo }
+						</a>
+					</td>
+					<td style=background-color:${ stl }; >
+						<a href=#${ FGA.urlGitHubPage }${ FGA.pathRepo }${ itemPath } title="${ item.size.toLocaleString() } bytes" >
+							${ item.name }
+						</a>
+						${ str }
+					</td>
+				</tr></table>
+			`;
+
+			// how to simplify
+			if ( ( !location.hash || location.hash.toLowerCase().endsWith( 'readme.md' ) )
+
+				&& ( item.name.toLowerCase() === 'readme.md' ) ) {
+
+				location.hash = FGA.urlGitHubPage + FGA.pathRepo + itemPath;
+
+			}
+
+		}
+
+	}
+
+	return htm;
+
+};
+
+
+
+FGA.setBreadcrumbs = function( path ) {
+	//console.log( 'path', path );
+
+	const htmHome =
+	`
+		<b>
+			<a href=JavaScript:FGA.setMenuGitHubPathFileNames(); title="home folder" >
+				${ ( FGA.pathRepo ? FGA.pathRepo : "<span style=font-size:28px >&#x2302</span>" ) }
+			</a> &raquo;
+		</b>
+	`;
+
+	const folders = path ? path.split( '/' ) : [] ;
+
+	let htmFolders = "";
+	let str = "";
+
+	for ( let folder of folders ) {
+
+		str += `${ folder }/`;
+
+		htmFolders += `<b><a href=JavaScript:FGA.setMenuGitHubPathFileNames("${ str.slice( 0, -1 ) }"); >${ folder }</a> &raquo; </b>`;
+
+	}
+
+	FGAdivBreadcrumbs.innerHTML = htmHome + htmFolders;
+
+};
---
+++ js-tootoo13-3-0/mnu-menu.js
@@ -0,0 +1,384 @@
+// Copyright 2019 pushMe-pullYou authors. MIT License
+/* global THREE * /
+/* jshint esversion: 6 */
+
+
+const MNU = { "release": "R13.2", "date": "2019-03-04" };
+
+
+MNU.description =
+	`
+		TooToo Menu (MNU) generates standard HTML TooToo menu code and content and code that works on computers, tablets and phones
+	`;
+
+////////// currentStatus items are for the question marks in the main HTML document
+
+	MNU.currentStatusMenu =
+	`
+		<h3>TooToo Menu (MNU) ${ MNU.release} ~ ${ MNU.date }</h3>
+
+		<p>${ MNU.description }</p>
+
+		<p>
+			Concept
+			<ul>
+				<li>Creates default menu header and footer content and code</li>
+				<li>Code for hamburger sliding menu</li>
+				<li>Code for pop-up window</li>
+				<li>Code to add and select and load theme stylesheets</li>
+			</ul>
+
+		</p>
+
+		<p>This module is ready for light testing.</p>
+
+		<p><a href="https://pushme-pullyou.github.io/tootoo13/tootoo13.html#cookbook/mnu-menu/README.md" target="_blank" >TooToo Menu Read Me</a></p>
+
+		<details>
+
+			<summary>Change log</summary>
+
+			<ul>
+				<li>2019-03-04 ~ 15.0 ~ update github mark</li>
+				<li>2019-02-13 ~ Add rate limits popup closer</li>
+				<li>2019-02-07 ~ R13/1.0 ~ refactor styles / code cleanup</li>
+				<li>2019-01-15 ~ Add MNU.description text content and code</li>
+				<li>2019-01-14 ~ Update text content</li>
+				<li>2019-01-12 ~ Add MNUdetFooter id and set to open in test HTML</li>
+				<li>2019-01-11 ~ Add close button to status pop-up and improve pop-up toggling</li>
+				<li>2019-01-09 ~ Content update and minor code fixes</li>
+				<li>2019-01-09 ~ Add vars: MNU.descriptionTooToo, MNU.footerUrl, MNU.footerTarget, MNU.footerIssues</li>
+				<li>2018-12-29 ~ Add helpItem class</li>
+				<li>2018-12-28 ~ Current Status link changed to question mark</li>
+				<li>2018-12-28 ~ Content displayed in the Pop-Up</li>
+				<li>2018-12-22 ~ Themes added</li>
+				<li>2018-12-22 ~ Update subtext</li>
+				<!-- <li></li>
+				-->
+			</ul>
+
+		</details>
+	`;
+
+
+////////// boilerplate for downstream users
+
+MNU.urlSourceCodeImage = "https://pushme-pullyou.github.io/github-mark-64.png";
+MNU.urlSourceCodeIcon = `<img src="${ MNU.urlSourceCodeImage }" height=18 style=opacity:0.5 >`;
+MNU.urlSourceCode = "https://github.com/pushme-pullyou/tootoo13/tree/master/js-tootoo13-1";
+
+
+MNU.descriptionTooToo =
+	`
+		<h3>
+			<a href="https://github.com/pushme-pullyou/tootoo13" target="_blank">${ MNU.urlSourceCodeIcon }</a>
+			<a href="https://pushme-pullyou.github.io#tootoo13/README.md" target="_blank">
+				TooToo13 Read Me
+			</a>
+		</h3>
+
+		<p>
+			<ul>
+				<li>TooToo is a collection of JavaScript scripts to help you browse and view the contents of many files on <a href="https://github.com">GitHub</a> with remarkable ease</li>
+				<li>View HTML, <a href="https://en.wikipedia.org/wiki/Markdown">Markdown</a>, images and many other file types as rendered web pages or as source code</li>
+				<li>Traverse a tree menu, select and display folder and file names in  with single clicks</li>
+				<li>Open local files with OS file dialog or by drag and drop and open remote files with a URL</li>
+				<li>Access files quickly and then edit and commit changes to source using the GitHub interface</li>
+				<li>Web app with sliding menu that works on computer, tablet or phone</li>
+				<li>Written in plain-vanilla JavaScript / Uses the <a href="https://developer.github.com/v3/">GitHub Developer API</a> / Hosting with <a href="https://pages.github.com/">GitHub Pages</a> / Simple CSS theme selection</li>
+			</ul>
+		</p>
+
+		<p>TooToo is is one of several apps in the <a href="https://pushme-pullyou.github.io" target="_blank">pushMe-pullYou</a> GitHub organization</p>
+	`;
+
+
+MNU.currentStatusCore =
+	`
+		<h3>MNU.currentStatusCore</h3>
+
+		<p>This web page is built with:</p>
+
+		${ MNU.descriptionTooToo }
+	`;
+
+
+
+// For main menu header
+MNU.homeText = "homeText";
+MNU.homeTitle = "homeTitle";
+MNU.homeUrl = "";
+
+MNU.repoText = "repoText";
+MNU.repoTitle = "repoTitle";
+MNU.repoUrl = "";
+
+MNU.appText = "appText";
+MNU.appTitle = "appTitle";
+MNU.appUrl = "";
+
+MNU.footerUrl = "#";
+MNU.footerTarget = ""; //"target=_blank";
+MNU.footerIssues = "https://github.com/pushme-pullyou/pushme-pullyou.github.io/issues";
+
+MNU.statusId = null;
+
+MNU.xDown = null;
+MNU.yDown = null;
+
+//////////
+
+
+MNU.getNavHeader = function() {
+
+	// Swipe events
+	document.addEventListener( 'touchstart', MNU.onTouchStart, false );
+	document.addEventListener( 'touchmove', MNU.onTouchMove, false );
+
+
+
+	const htm  =
+	`
+	<div class=navSubMenu >
+		<h3>
+		<a href="${ MNU.homeUrl }" title="${ MNU.homeTitle }" target="_top">
+		${ MNU.homeText }
+		</a>
+		${ MNU.homeText ? '&raquo;' : '' }
+		<a href="${ MNU.repoUrl }" title="${ MNU.repoTitle }" target="_top">
+		${ MNU.repoText }
+		</a>
+		${ MNU.repoText ? '&raquo;' : '' }
+		<a href="${ MNU.appUrl }" title="${ MNU.appTitle }" >
+		${ MNU.appText }
+		</a>
+		${ MNU.appText ? '&raquo;' : '' }
+		</h3>
+		<h2>
+			<a href=${ MNU.urlSourceCode } ${ MNU.footerTarget } title="Source code on GitHub" >
+			${ MNU.urlSourceCodeIcon }
+			</a>
+			<a href="" title="Click to reload this page" >${ document.title } <span id=titleRelease >R${ document.head.querySelector( '[ name=release ]' ).content }</span></a>
+
+			<a id=mnuCore class=helpItem href="JavaScript:MNU.setPopupShowHide(mnuCore,MNU.currentStatusCore);"
+				title="Current status: core module" >&nbsp; ? &nbsp;
+			</a>
+		</h3>
+
+		<p>
+			${ MNU.description } <a id=mnuHead class=helpItem href="JavaScript:MNU.setPopupShowHide(mnuHead,MNU.currentStatusMenu);"
+				title="Current status" >&nbsp; ? &nbsp;
+			</a>
+		</p>
+
+	</div>
+	`;
+
+	return htm;
+
+};
+
+
+
+MNU.getNavFooter = function() {
+
+	const htm  =
+	`
+		<details id=MNUdetFooter >
+
+			<summary class=sumMenuTitle >Help menu
+				<a id=mnuFoot class=helpItem href="JavaScript:MNU.setPopupShowHide(mnuFoot,MNU.currentStatusMenu);" >&nbsp; ? &nbsp;</a>
+			</summary>
+
+			<div title='many thanks!' ><a href=${ MNU.footerUrl }pages/about-tootoo.md ${ MNU.footerTarget } >About TooToo</a></div>
+			<div title='many thanks!' ><a href=${ MNU.footerUrl }pages/credits.md ${ MNU.footerTarget } >Credits</a></div>
+			<div><a href=${ MNU.footerUrl }pages/code-of-conduct.md ${ MNU.footerTarget } >Code of conduct</a></div>
+			<div><a href=${ MNU.footerUrl }pages/contributing.md ${ MNU.footerTarget } >Contributing via GitHub</a></div>
+			<div><a href=${ MNU.footerUrl }pages/license.md ${ MNU.footerTarget } >MIT License</a></div>
+			<div><a href=${ MNU.footerUrl }pages/markdown-help.md ${ MNU.footerTarget } >Markdown help</a></div>
+			<div><a href=${ MNU.footerUrl }pages/themes.md ${ MNU.footerTarget } >Themes help</a></div>
+			<div>&raquo; <a title='Need help' href=${ MNU.footerIssues } target=_blank >${ MNU.repoText } GitHub Issues</a></div>
+			<div><button onclick=MNU.showFps() >Show frames/second statistics</button></div>
+			<div><button id=MNUbutRateLimits onclick=MNU.rateLimits(); >View GitHub API rate limits</button>
+			<hr>
+
+		</details>
+	`;
+
+	return htm;
+
+};
+
+
+MNU.showFps = function(){
+
+	const script = document.body.appendChild( document.createElement('script') );
+
+	script.onload = function() {
+
+		MNU.stats = new Stats();
+
+		document.body.appendChild( MNU.stats.dom );
+
+		loop();
+
+	}
+
+	script.src='https://rawgit.com/mrdoob/stats.js/master/build/stats.min.js';
+
+	function loop(){
+
+		MNU.stats.update();
+		requestAnimationFrame( loop );
+
+	}
+
+};
+
+
+MNU.rateLimits = function() {
+
+	const url = "https://api.github.com/rate_limit";
+
+	const xhr = new XMLHttpRequest();
+	xhr.open( 'GET', url, true );
+	xhr.onload = callback;
+	xhr.send( null );
+
+	function callback( xhr ) {
+
+		const text =
+		`
+			<h3>GitHub rate limits status</h3>
+
+			<p>
+				Some TooToo scripts use the GitHub Developer API which has rate limits.
+			</p>
+
+			<p>See <a href="https://developer.github.com/v3/#rate-limiting" target="_blank">developer.github.com/v3</a>.</p>
+
+			<pre> ${ xhr.target.response } </pre>
+		`;
+
+		MNU.setPopupShowHide( MNUbutRateLimits, text );
+
+	}
+
+}
+
+
+
+//////////
+
+MNU.setPopupShowHide = function( id, text ) {
+	//console.log( 'id', id );
+
+	MNU.statusId = id;
+
+	id.classList.toggle( "active" );
+
+	if ( id.classList.contains( 'active' ) ) {
+
+		const closer = `<div style=text-align:right; ><button onclick=MNU.setPopupShowHide(MNU.statusId,""); >X</button></div>`;
+
+		divPopUpData.innerHTML = id.classList.contains( 'active' ) ? closer + text : '';
+
+		divContainer.addEventListener( 'click', MNU.onClickContainer, false );
+
+	} else {
+
+		divContainer.removeEventListener( 'click', MNU.onClickContainer, false );
+
+	}
+
+};
+
+
+
+MNU.onClickContainer = function() {
+
+	divPopUpData.innerHTML = "";
+
+};
+
+
+//////////
+
+MNU.onTouchStart = function( event ) {
+
+	MNU.xDown = event.touches[ 0 ].clientX;
+	MNU.yDown = event.touches[ 0 ].clientY;
+
+};
+
+
+
+MNU.onTouchMove = function(event) {
+
+	// if ( ! MNU.xDown || ! MNU.yDown ) { return; }
+
+	const xUp = event.touches[ 0 ].clientX;
+	const yUp = event.touches[ 0 ].clientY;
+
+	const xDiff = MNU.xDown - xUp;
+	const yDiff = MNU.yDown - yUp;
+
+	if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {// most significant
+
+		if ( xDiff > 0 ) {
+
+			MNU.toggleNavLeft();
+			//console.log( 'left swipe' );
+
+		} else {
+
+			MNU.toggleNavLeft();
+			console.log( 'right swipe' );
+
+		}
+
+	} else {
+
+		if ( yDiff > 0 ) {
+
+			console.log( 'up swipe' );
+
+		} else {
+
+			console.log( 'down swipe' );
+
+		}
+
+	}
+
+	MNU.xDown = null;
+	MNU.yDown = null;
+
+};
+
+
+
+MNU.toggleNavLeft = function() {
+
+	const width = getComputedStyle(document.documentElement).getPropertyValue( '--mnu-width' ).trim();
+
+	//console.log( 'width', width );
+	//console.log( 'navMenu.style.width', navMenu.style.width );
+
+	if ( navMenu.style.width === "0px" ) { // invisible
+
+		navMenu.style.width = width;
+		//navMenu.style.padding = '1rem';
+		butHamburger.style.left = 'var( --mnu-width )';
+		divContainer.style.marginLeft = width;
+
+	} else {
+
+		navMenu.style.width = "0px";
+		//navMenu.style.padding = '0';
+		butHamburger.style.left = '-3rem';
+		divContainer.style.marginLeft = "0px";
+
+	}
+
+}
